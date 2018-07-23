0bffba3c84768db51f443a16255f8a80ec6f2b14
Mark Purcell <MarkPurcell@ie.ibm.com>
2018-07-23 17:04:35 +0100

    No need to specify Bluemix space installing actions

--- openwhisk/install.sh
+++ openwhisk/install.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 #Author: Mark Purcell (markpurcell@ie.ibm.com)
 
-if [[ ! $# -eq 1 ]] ; then
-	echo "Usage: Bluemix space (e.g. dev)"
-        exit 0
-fi
-
 if [[ ! -f my_setup.sh ]]; then
-	echo "Must run provision.sh first " $1
+	echo "Must run provision.sh first "
 	exit 1
 fi
 
@@ -18,15 +13,15 @@ DIR=.
 DBVERSION=$(cat $DIR/VERSION)
 
 if [[ ! -f $DIR/get_ts-$DBVERSION.zip ]]; then
-	echo "Must run make build first " $1
+	echo "Must run make build first "
 	exit 1
 fi
 if [[ ! -f $DIR/insert-$DBVERSION.zip ]]; then
-	echo "Must run make build first " $1
+	echo "Must run make build first "
 	exit 1
 fi
 if [[ ! -f $DIR/get_devices-$DBVERSION.zip ]]; then
-	echo "Must run make build first " $1
+	echo "Must run make build first "
 	exit 1
 fi
 
@@ -63,6 +58,7 @@ if [ -n "$temp" ]; then
     bx wsk rule delete $temp
 fi
 
-bx wsk trigger create $CRISTATA_WSK_TRIGGER$DBVERSION --feed $WSK_FEED --param topic $TOPIC_MHUB
-bx wsk rule create $CRISTATA_WSK_RULE$DBVERSION $CRISTATA_WSK_TRIGGER$DBVERSION $WSK_ADD
+increment=$RANDOM
+bx wsk trigger create $CRISTATA_WSK_TRIGGER$increment --feed $WSK_FEED --param topic $TOPIC_MHUB
+bx wsk rule create $CRISTATA_WSK_RULE$increment $CRISTATA_WSK_TRIGGER$increment $WSK_ADD
 
